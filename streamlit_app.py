49a7065ddbb96553ce472aa140288fdab6a932e8
mohammedabarka-coder <mohammed.a.barka@gmail.com>
2025-12-16 14:11:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import numpy as np
+import pickle
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+model = pickle.load(open("model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
+
+st.title("House Price Prediction")
+
+MedInc = st.number_input("Median Income", min_value=0.0)
+HouseAge = st.number_input("House Age", min_value=0.0)
+AveRooms = st.number_input("Average Rooms", min_value=0.0)
+AveBedrms = st.number_input("Average Bedrooms", min_value=0.0)
+Population = st.number_input("Population", min_value=0.0)
+AveOccup = st.number_input("Average Occupancy", min_value=0.0)
+Latitude = st.number_input("Latitude")
+Longitude = st.number_input("Longitude")
+
+if st.button("Predict"):
+    data = np.array([[MedInc, HouseAge, AveRooms, AveBedrms,
+                      Population, AveOccup, Latitude, Longitude]])
+    data = scaler.transform(data)
+    result = model.predict(data)
+
+    st.success(f"Estimated House Price: {result[0]:.2f}")
