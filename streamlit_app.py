12e8a3bc3a277aa201bed31a0d2b50abcad98d97
mohammedabarka-coder <mohammed.a.barka@gmail.com>
2025-12-16 22:58:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,28 +10,28 @@ st.title("🏠 House Price Prediction")
 
 # Intervalles logiques et contrôlés
 MedInc = st.number_input(
-    "Median Income", min_value=0.5, max_value=15.0, value=3.0)
+    "Median Income", min_value=0.5, max_value=15.0, value=0)
 
 HouseAge = st.number_input(
-    "House Age (years)", min_value=1.0, max_value=52.0, value=20.0)
+    "House Age (years)", min_value=1.0, max_value=52.0, value=0)
 
 AveRooms = st.number_input(
-    "Average Rooms", min_value=1.0, max_value=10.0, value=5.0)
+    "Average Rooms", min_value=1.0, max_value=10.0, value=0)
 
 AveBedrms = st.number_input(
-    "Average Bedrooms", min_value=1.0, max_value=5.0, value=2.0)
+    "Average Bedrooms", min_value=1.0, max_value=5.0, value=0)
 
 Population = st.number_input(
-    "Population", min_value=1.0, max_value=100.0, value=50.0)
+    "Population", min_value=1.0, max_value=10000.0, value=0)
 
 AveOccup = st.number_input(
-    "Average Occupancy", min_value=1.0, max_value=5.0, value=3.0)
+    "Average Occupancy", min_value=1.0, max_value=5.0, value=0)
 
 Latitude = st.number_input(
-    "Latitude", min_value=32.0, max_value=42.0, value=37.0)
+    "Latitude", min_value=32.0, max_value=42.0, value=0)
 
 Longitude = st.number_input(
-    "Longitude", min_value=-124.0, max_value=-114.0, value=-122.0)
+    "Longitude", min_value=-124.0, max_value=-114.0, value=0)
 
 if st.button("Predict"):
     data = np.array([[MedInc, HouseAge, AveRooms, AveBedrms,
